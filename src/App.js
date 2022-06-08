e62891ca61e75938933c7a3df441c604c19e44c9
admin-tobeweb <reouven.tobeweb@gmail.com>
2022-06-08 21:25:18 +0300

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ConnectModal from './components/ConnectModal';
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from './utils/firebase.config';
+import CreatePost from './components/CreatePost';
+const App = () => {
+  const [user, setUser] = useState(null);
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  })
 
-function App() {
+  const handleLogout = async ()=>{
+    await signOut(auth);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app-header">
+        {user && (
+          <div className="user-infos">
+            <span>{user?.displayName[0]}</span>
+            <h4>{user?.displayName}</h4>
+            <button onClick={()=>handleLogout()}><i className='fa-solid fa-arrow-right-from-bracket'></i></button>
+          </div>
+        )}
+        {user ? (<CreatePost />) : (<ConnectModal />)}
+        
+      </div>
+      <div className="posts-container"></div>
     </div>
   );
-}
+};
 
 export default App;
